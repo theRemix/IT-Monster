c37c18b37bf26c734a3efbfc6d80863bf1749248
Kent Salcedo <kent@sudokrew.com>
2018-06-19 10:52:18 -1000

    Update slides - Add happy stack to last app slide - Move exploding IT monster to its own slide

--- src/slides/Slide5.jsx
+++ src/slides/Slide5.jsx
@@ -3,105 +3,14 @@ import React from 'react';
 // Require CSS
 require('./slide5.css');
 
-function format(float) {
-  return float
-    .toFixed()
-    .toString()
-    .split('')
-    .reduce(function(number, digit, i, a) {
-      return (
-        number +
-        (a.length - 1 - i % 3 === 3 && i < a.length - 1 ? digit + ',' : digit)
-      );
-    }, '');
-}
-
 export class Slide5 extends React.Component {
-  state = {
-    animating: false,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.distance = React.createRef();
-    this.monster = React.createRef();
-    this.label = React.createRef();
-    this.moonDistance = 238900;
-  }
-
-  animate = timestamp => {
-    if (
-      this.distance.current !== null &&
-      this.monster.current !== null &&
-      this.label.current !== null
-    ) {
-      if (this.state.animating) {
-        var stepDistance =
-          this.distance.current.clientWidth /
-          (0.52 * document.body.clientWidth) *
-          this.moonDistance;
-        this.label.current.innerText =
-          stepDistance > this.moonDistance / 2
-            ? format(stepDistance) + ' mi'
-            : format(stepDistance);
-        window.requestAnimationFrame(this.animate);
-      } else {
-        this.label.current.innerText = format(this.moonDistance) + ' mi';
-        this.monster.current.classList.add('monster-animate');
-      }
-    }
-  };
-
-  setAnimating = () => {
-    this.setState({ animating: true });
-    window.requestAnimationFrame(this.animate);
-  };
-
-  setNotAnimating = () => {
-    this.setState({ animating: false });
-  };
-
-  componentDidMount() {
-    this.distance.current.addEventListener('animationstart', this.setAnimating);
-    this.distance.current.addEventListener(
-      'animationend',
-      this.setNotAnimating
-    );
-  }
-
-  componentWillUnmount() {
-    this.distance.current.removeEventListener(
-      'animationstart',
-      this.setAnimating
-    );
-    this.distance.current.removeEventListener(
-      'animationend',
-      this.setNotAnimating
-    );
-  }
-
   render() {
     return (
-      <div className="wrapper">
-        <h1>Current Distance to the Moon</h1>
-        <div className="container">
-          <div className="earth">
-            <span>earth</span>
-          </div>
-          <div className="distance long" ref={this.distance}>
-            <span className="distance-label" ref={this.label} />
-          </div>
-          <div className="moon">
-            <span>moon</span>
-          </div>
-        </div>
-
+      <div>
         <img
-          ref={this.monster}
-          className="monster"
+          id="monster05"
           src="./assets/monster_explodes.svg"
-          alt="IT Monster"
+          alt="IT monster explodes"
         />
       </div>
     );
